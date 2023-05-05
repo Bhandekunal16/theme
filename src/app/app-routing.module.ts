e0bf7b35ec151d6dc693e39a5d1ab9a2803c1bf0
bhandekunal16 <bhandekunal16@gmail.com>
2023-05-05 11:21:56 +0530

    create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoxComponent } from './box/box.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: BoxComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
